8d145973b94f7ee7b489b3814f392d21000f1f46
Hasan CAN <hasancan.net.tr@gmail.com>
2021-06-05 09:31:12 +0300

    Create text-to-hw.py

---
+++ text-to-hw.py
@@ -0,0 +1,3 @@
+import pywhatkit as handwrite
+
+handwrite.text_to_handwriting("You can automatically convert any text you enter in this field to handwriting. You can adjust the color of the handwriting to be created by editing the color codes at the end of the text. When you run this file, an image file containing the handwritten version of the text entered in this field will be created in the file directory.", rgb=(0, 0, 255))
